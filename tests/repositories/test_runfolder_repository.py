290ca28e166e2ad00b7f8297a1c9879ac3996b7d
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-10-17 14:44:52 +0200

    Move mocking to utility class

--- tests/repositories/test_runfolder_repository.py
+++ tests/repositories/test_runfolder_repository.py
@@ -7,47 +7,18 @@ from models.runfolder import Runfolder
 from models.project import Project
 from delivery.repositories.runfolder_repository import FileSystemBasedRunfolderRepository
 
+from tests.test_utils import FAKE_RUNFOLDERS, mock_file_system_service, fake_directories, fake_projects
+
 
 class TestRunfolderRepository(unittest.TestCase):
 
-    fake_directories = ["160930_ST-E00216_0111_BH37CWALXX", "160930_ST-E00216_0112_BH37CWALXX"]
-    fake_projects = ["ABC_123", "DEF_456"]
-
-    @staticmethod
-    def _mock_file_system_service(fake_directories, fake_projects):
-
-        mock_file_system_service = MagicMock()
-        mock_file_system_service.find_runfolder_directories.return_value = fake_directories
-        mock_file_system_service.find_project_directories.return_value = fake_projects
-        return mock_file_system_service
-
-    runfolder1 = Runfolder(name="160930_ST-E00216_0111_BH37CWALXX",
-                           path="/foo/160930_ST-E00216_0111_BH37CWALXX")
-
-    runfolder1.projects = [Project(name="ABC_123",
-                                   path="/foo/160930_ST-E00216_0111_BH37CWALXX/Projects/ABC_123",
-                                   runfolder=runfolder1),
-                           Project(name="DEF_456",
-                                   path="/foo/160930_ST-E00216_0111_BH37CWALXX/Projects/DEF_456",
-                                   runfolder=runfolder1)]
-
-    runfolder2 = Runfolder(name="160930_ST-E00216_0112_BH37CWALXX",
-                           path="/foo/160930_ST-E00216_0112_BH37CWALXX")
-
-    runfolder2.projects = [Project(name="ABC_123",
-                                   path="/foo/160930_ST-E00216_0112_BH37CWALXX/Projects/ABC_123",
-                                   runfolder=runfolder1),
-                           Project(name="DEF_456",
-                                   path="/foo/160930_ST-E00216_0112_BH37CWALXX/Projects/DEF_456",
-                                   runfolder=runfolder1)]
-
-    expected_runfolders = [runfolder1, runfolder2]
+    expected_runfolders = FAKE_RUNFOLDERS
 
     def test_get_runfolders(self):
-        mock_file_system_service = TestRunfolderRepository._mock_file_system_service(self.fake_directories,
-                                                                                     self.fake_projects)
+        file_system_service = mock_file_system_service(fake_directories,
+                                                       fake_projects)
         repo = FileSystemBasedRunfolderRepository(base_path="/foo",
-                                                  file_system_service=mock_file_system_service)
+                                                  file_system_service=file_system_service)
         actual_runfolders = list(repo.get_runfolders())
 
         self.assertListEqual(self.expected_runfolders, actual_runfolders)
@@ -59,11 +30,11 @@ class TestRunfolderRepository(unittest.TestCase):
 
     def test_get_runfolders_does_not_return_none_runfolder(self):
         # Adding a directory which does not conform to the runfolder pattern
-        with_non_runfolder_dir = self.fake_directories + ["bar"]
-        mock_file_system_service = TestRunfolderRepository._mock_file_system_service(with_non_runfolder_dir,
-                                                                                     self.fake_projects)
+        with_non_runfolder_dir = fake_directories + ["bar"]
+        file_system_service = mock_file_system_service(with_non_runfolder_dir,
+                                                       fake_projects)
         repo = FileSystemBasedRunfolderRepository(base_path="/foo",
-                                                  file_system_service=mock_file_system_service)
+                                                  file_system_service=file_system_service)
         actual_runfolders = list(repo.get_runfolders())
         self.assertListEqual(self.expected_runfolders, actual_runfolders)
 
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,8 @@
 
+from models.project import Project
+from models.runfolder import Runfolder
+
+from mock import MagicMock
 
 class TestUtils:
     DUMMY_CONFIG = {}
@@ -7,4 +11,38 @@ class TestUtils:
 class DummyConfig:
     def __getitem__(self, key):
         return TestUtils.DUMMY_CONFIG[key]
+
+fake_directories = ["160930_ST-E00216_0111_BH37CWALXX", "160930_ST-E00216_0112_BH37CWALXX"]
+fake_projects = ["ABC_123", "DEF_456"]
+
+
+def mock_file_system_service(directories, projects):
+    mock_file_system_service_instance = MagicMock()
+    mock_file_system_service_instance.find_runfolder_directories.return_value = directories
+    mock_file_system_service_instance.find_project_directories.return_value = projects
+    return mock_file_system_service_instance
+
+_runfolder1 = Runfolder(name="160930_ST-E00216_0111_BH37CWALXX",
+                        path="/foo/160930_ST-E00216_0111_BH37CWALXX")
+
+_runfolder1.projects = [Project(name="ABC_123",
+                                path="/foo/160930_ST-E00216_0111_BH37CWALXX/Projects/ABC_123",
+                                runfolder=_runfolder1),
+                       Project(name="DEF_456",
+                               path="/foo/160930_ST-E00216_0111_BH37CWALXX/Projects/DEF_456",
+                               runfolder=_runfolder1)]
+
+_runfolder2 = Runfolder(name="160930_ST-E00216_0112_BH37CWALXX",
+                        path="/foo/160930_ST-E00216_0112_BH37CWALXX")
+
+_runfolder2.projects = [Project(name="ABC_123",
+                               path="/foo/160930_ST-E00216_0112_BH37CWALXX/Projects/ABC_123",
+                               runfolder=_runfolder2),
+                       Project(name="DEF_456",
+                               path="/foo/160930_ST-E00216_0112_BH37CWALXX/Projects/DEF_456",
+                               runfolder=_runfolder2)]
+
+
+FAKE_RUNFOLDERS = [_runfolder1, _runfolder2]
+
 
