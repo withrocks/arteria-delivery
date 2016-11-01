343c5eb47510f784588e425619c43df916a40fe7
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-11-01 18:18:09 +0100

    Remove at exit handler it doesnt work...

--- delivery/services/external_program_service.py
+++ delivery/services/external_program_service.py
@@ -1,6 +1,5 @@
 
 import subprocess
-import atexit
 
 from delivery.models.execution import ExecutionResult, Execution
 
@@ -13,9 +12,6 @@ class ExternalProgramService():
                              stdout=subprocess.PIPE,
                              stderr=subprocess.PIPE,
                              stdin=subprocess.PIPE)
-        # On exiting the main program, make sure that the subprocess
-        # gets killed.
-        atexit.register(p.terminate)
         return Execution(pid=p.pid, process_obj=p)
 
     @staticmethod
