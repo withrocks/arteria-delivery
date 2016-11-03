9e937cdb8372666a1e442fadad5609f89799aa44
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-11-03 17:47:02 +0100

    Staging exposed in REST API

--- delivery/app.py
+++ delivery/app.py
@@ -1,15 +1,19 @@
 
 from tornado.web import URLSpec as url
 
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+
 from arteria.web.app import AppService
 
 from delivery.handlers.utility_handlers import VersionHandler
 from delivery.handlers.runfolder_handlers import RunfolderHandler
 from delivery.handlers.project_handlers import ProjectHandler, ProjectsForRunfolderHandler
 from delivery.handlers.delivery_handlers import DeliverRunfolderHandler
-from delivery.handlers.staging_handlers import StagingRunfolderHandler
+from delivery.handlers.staging_handlers import StagingRunfolderHandler, StagingHandler
 
 from delivery.repositories.runfolder_repository import FileSystemBasedRunfolderRepository
+from delivery.repositories.staging_repository import DatabaseBasedStagingRepository
 
 from delivery.services.delivery_service import MoverDeliveryService
 from delivery.services.external_program_service import ExternalProgramService
@@ -35,6 +39,9 @@ def routes(**kwargs):
         url(r"/api/1.0/stage/runfolder/(.+)", StagingRunfolderHandler,
             name="stage_runfolder", kwargs=kwargs),
 
+        url(r"/api/1.0/stage/(\d+)", StagingHandler, name="stage_status", kwargs=kwargs),
+
+        # TODO Should deliver by stage id!
         url(r"/api/1.0/deliver/runfolder/(.+)", DeliverRunfolderHandler,
             name="delivery_runfolder", kwargs=kwargs)
     ]
@@ -45,13 +52,26 @@ def start():
     Start the delivery-ws app
     """
     app_svc = AppService.create(__package__)
-
     config = app_svc.config_svc
+
     runfolder_repo = FileSystemBasedRunfolderRepository(
         config["monitored_directory"])
     external_program_service = ExternalProgramService()
+
+    db_connection_string = config["db_connection_string"]
+    engine = create_engine(db_connection_string, echo=False)
+
+    session_factory = scoped_session(sessionmaker())
+    session_factory.configure(bind=engine)
+
+    staging_repo = DatabaseBasedStagingRepository(session_factory=session_factory)
+
     staging_service = StagingService(external_program_service=external_program_service,
-                                     staging_dir=config["staging_directory"])
+                                     runfolder_repo=runfolder_repo,
+                                     staging_repo=staging_repo,
+                                     staging_dir=config["staging_directory"],
+                                     session_factory=session_factory)
+
     delivery_service = MoverDeliveryService(external_program_service)
 
     app_svc.start(routes(config=config,
