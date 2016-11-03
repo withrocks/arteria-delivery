128d4b404852ca953d939294916a648ef103f9da
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-11-03 17:50:35 +0100

    StagingRepo session factory as input

--- delivery/repositories/staging_repository.py
+++ delivery/repositories/staging_repository.py
@@ -1,4 +1,6 @@
 
+from sqlalchemy.orm.exc import NoResultFound
+
 from delivery.models.deliveries import StagingOrder
 
 
@@ -19,17 +21,26 @@ class BaseStagingRepository(object):
 
 class DatabaseBasedStagingRepository(BaseStagingRepository):
 
-    def __init__(self, session):
-        self.session = session
+    def __init__(self, session_factory):
+        self.session = session_factory()
 
     def get_staging_order_by_source(self, source):
         return self.session.query(StagingOrder).filter(StagingOrder.source == source).all()
 
-    def get_staging_order_by_id(self, identifier):
-        return self.session.query(StagingOrder).filter(StagingOrder.id == identifier).one()
+    def get_staging_order_by_id(self, identifier, custom_session=None):
+        if custom_session:
+            session = custom_session
+        else:
+            session = self.session
+        try:
+            return session.query(StagingOrder).filter(StagingOrder.id == identifier).one()
+        except NoResultFound:
+            return None
 
     def create_staging_order(self, source, status):
+
         order = StagingOrder(source=source, status=status)
         self.session.add(order)
         self.session.commit()
+
         return order
