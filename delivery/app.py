71b04eaf831dbba80c49d4bdf354dae24e13a169
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-11-07 13:40:46 +0100

    The very first alembic configuration
    This will create the database once the application is started, and run
    migration scripts as necessary to ensure that the database is up to date
    with the application code.

--- delivery/app.py
+++ delivery/app.py
@@ -4,6 +4,9 @@ from tornado.web import URLSpec as url
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, scoped_session
 
+from alembic.config import Config as AlembicConfig
+from alembic.command import upgrade as upgrade_db
+
 from arteria.web.app import AppService
 
 from delivery.handlers.utility_handlers import VersionHandler
@@ -47,6 +50,16 @@ def routes(**kwargs):
     ]
 
 
+def create_and_migrate_db(db_engine, db_connection_string):
+    # TODO Do not hard code
+    alembic_cfg = AlembicConfig('config/alembic.ini')
+    alembic_cfg.set_main_option("url", db_connection_string)
+    alembic_cfg.set_main_option("script_location", "alembic/")
+
+    with db_engine.begin() as connection:
+        alembic_cfg.attributes["connection"] = connection
+        upgrade_db(alembic_cfg, "head")
+
 def compose_application(config):
     runfolder_repo = FileSystemBasedRunfolderRepository(
         config["monitored_directory"])
@@ -54,6 +67,8 @@ def compose_application(config):
 
     db_connection_string = config["db_connection_string"]
     engine = create_engine(db_connection_string, echo=False)
+
+    create_and_migrate_db(engine, db_connection_string)
 
     session_factory = scoped_session(sessionmaker())
     session_factory.configure(bind=engine)
