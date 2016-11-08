53ac05898fb08615b83e7b6e0985af8c1514b995
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-11-08 11:02:17 +0100

    Adds docs to models and makes sure path is abs.

--- delivery/models/project.py
+++ delivery/models/project.py
@@ -4,14 +4,28 @@ from delivery.models import BaseModel
 
 
 class Project(BaseModel):
+    """
+    Model a project directory in a runfolder on disk. Note that this means that this project model only extends
+    to the idea of projects as subdirectories in a demultiplexed Illumina runfolder.
+    """
 
     def __init__(self, name, path, runfolder_path=None):
+        """
+        Instantiate a new project object
+        :param name: of the project
+        :param path: path to the project
+        :param runfolder_path: path the runfolder in which this project is stored.
+        """
         self.name = name
-        self.path = path
+        self.path = os.path.abspath(path)
         self.runfolder_path = runfolder_path
 
     def __eq__(self, other):
+        """
+        Two project should be considered the same if the represent the same directory on disk
+        :param other: instance of Project
+        :return: true if the same project, otherwise false
+        """
         if isinstance(other, self.__class__):
-            # TODO Make sure the property is abspath
-            return os.path.abspath(self.path) == os.path.abspath(other.path)
+            return self.path == other.path
         return False
--- delivery/models/runfolder.py
+++ delivery/models/runfolder.py
@@ -5,13 +5,27 @@ from delivery.models import BaseModel
 
 
 class Runfolder(BaseModel):
+    """
+    Models the concept of a runfolder on disk
+    """
 
     def __init__(self, name, path, projects=None):
+        """
+        Instantiate a new runfolder instance
+        :param name: of the runfolder
+        :param path: to the runfolder
+        :param projects: all projects which are located under this runfolder
+        """
         self.name = name
-        self.path = path
+        self.path = os.path.abspath(path)
         self.projects = projects
 
     def __eq__(self, other):
+        """
+        Two runfolders should be considered the same if the represent the same directory on disk
+        :param other: runfolder instance to compare to
+        :return: True if the represent the same folder on disk, otherwise false.
+        """
         if isinstance(other, self.__class__):
-            return os.path.abspath(self.path) == os.path.abspath(other.path)
+            return self.path == other.path
         return False
